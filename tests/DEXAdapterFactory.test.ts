7ff8d4c5091fb9e3b69a67edf24ecdfe3bb541a1
Andrii Voloshchuk <avoloshchuk@fairwaydev.com>
2025-05-06 15:14:33 +0200

    feat: add type checking tests for DEXAdapterFactory and update exports

--- tests/DEXAdapterFactory.test.ts
+++ tests/DEXAdapterFactory.test.ts
@@ -1,17 +1,40 @@
 import { DEXAdapterFactory } from '../src/factory/DEXAdapterFactory';
 import { IDEXAdapter } from '../src/interfaces/IDEXAdapter';
+import { PumpSwapAdapter } from '../src/adapters/PumpSwapAdapter';
+import { RaydiumAMMAdapter } from '../src/adapters/RaydiumAMMAdapter';
+import { RaydiumCPMMAdapter } from '../src/adapters/RaydiumCPMMAdapter';
+import { RaydiumCLMNAdapter } from '../src/adapters/RaydiumCLMNAdapter';
+import { MeteoraDLMNAdapter } from '../src/adapters/MeteoraDLMNAdapter';
 
 describe('DEXAdapterFactory', () => {
   it('should return PumpSwap adapter', () => {
     const adapter = DEXAdapterFactory.getAdapter('pumpswap');
     expect(adapter).toBeDefined();
-    expect(adapter).toBeInstanceOf(Object);
+    expect(adapter).toBeInstanceOf(PumpSwapAdapter);
   });
 
   it('should return Raydium AMM adapter', () => {
     const adapter = DEXAdapterFactory.getAdapter('raydium-amm');
     expect(adapter).toBeDefined();
-    expect(adapter).toBeInstanceOf(Object);
+    expect(adapter).toBeInstanceOf(RaydiumAMMAdapter);
+  });
+
+  it('should return Raydium CPMM adapter', () => {
+    const adapter = DEXAdapterFactory.getAdapter('raydium-cpmm');
+    expect(adapter).toBeDefined();
+    expect(adapter).toBeInstanceOf(RaydiumCPMMAdapter);
+  });
+
+  it('should return Raydium CLMN adapter', () => {
+    const adapter = DEXAdapterFactory.getAdapter('raydium-clmn');
+    expect(adapter).toBeDefined();
+    expect(adapter).toBeInstanceOf(RaydiumCLMNAdapter);
+  });
+
+  it('should return Meteora DLMN adapter', () => {
+    const adapter = DEXAdapterFactory.getAdapter('meteora-dlmn');
+    expect(adapter).toBeDefined();
+    expect(adapter).toBeInstanceOf(MeteoraDLMNAdapter);
   });
 
   it('should throw error for unknown adapter', () => {
